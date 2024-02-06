74568288d8af23b11dc52f17bc88a1b68b1925eb
susheel <sky019813@gmail.com>
2024-02-06 18:02:53 +0530

    susheel

--- routes/customer.js
+++ routes/customer.js
@@ -14,50 +14,45 @@ router.get("/", (req, res) => {
 
 router.post("/", urlencodedParser, async (req, res) => {
   //we need data
+  if ("btnAdd" in req.body) {
+    var cus = new Customer();
+    cus.id = parseInt(req.body.txtId);
+    cus.name = req.body.txtName;
+    cus.address = req.body.txtAddress;
+    cus.mobile = req.body.txtMobile;
 
-  // console.log(req.body);
-  if('btnAdd' in req.body){
-  var cus = new Customer();
-  cus.id = parseInt(req.body.txtId);
-  cus.name = req.body.txtName;
-  cus.address = req.body.txtAddress;
-  cus.mobile = req.body.txtMobile;
-
-  var result = cus.addCustomer();
-  result.then(
-    (cus) => {
+    var result = cus.addCustomer();
+    result.then(
+      (cus) => {
+        var p = Customer.showAllCustomer();
+        p.then(
+          (data) => {
+            res.render("Customer", { cusArr: data });
+          },
+          (err) => {
+            res.render("Customer", { cus: err });
+          }
+        );
+      },
+      (err) => {
+        res.render("Customer", { cusArr: [], err: err });
+      }
+    );
+  } else if ("btnModify" in req.body) {
+    var cus = new Customer();
+    cus.id = req.body.txtId;
+    cus.name = req.body.txtName;
+    cus.address = req.body.txtAddress;
+    cus.mobile = req.body.txtMobile;
+    var cus = cus.modifyCustomer(cus.id);
+    cus.then((data) => {
       var p = Customer.showAllCustomer();
-      p.then(
-        (data) => {
-          res.render("Customer", { cusArr: data });
-        },
-        (err) => {
-          res.render("Customer", { cus: err });
-        }
-      );
-    },
-    (err) => {
-      res.render("Customer", { cusArr: [], err: err });
-    }
-  );
-}else if('btnModify' in req.body){
-  var cus = new Customer();
-  cus.id = req.body.txtId
-  cus.name = req.body.txtName;
-  cus.address = req.body.txtAddress;
-  cus.mobile = req.body.txtMobile;
-  var cus = cus.modifyCustomer(cus.id);
-  cus.then((data) => {
-    var p = Customer.showAllCustomer();
-    p.then((data) => {
-      res.render("Customer", { cusArr: data });
+      p.then((data) => {
+        res.render("Customer", { cusArr: data });
+      });
     });
-  });
-  
-
-}
-}
-);
+  }
+});
 
 router.get("/search/:id", (req, res) => {
   var cus = new Customer();
--- t1.js
+++
@@ -1,6 +0,0 @@
-var obj ={id:5 , name:'abc4' , address:'noida'}
-if('id' in obj){
-    console.log('id');
-}else{
-    console.log('id not found');
-}
